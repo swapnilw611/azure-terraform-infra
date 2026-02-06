8f04bd276e7007787c5efa805a24aa18ae5f20fa
swapnilw611 <swapnilw611@gmail.com>
2026-02-06 13:19:47 +0530

    add veriables

---
+++ azure/variable.tf
@@ -0,0 +1,15 @@
+variable "ssh_public_key_path" {
+  description = "Path to your SSH public key"
+}
+
+variable "admin_username" {
+  default = "admin"
+}
+
+variable "admin_password" {
+  default = "Admin@123"
+}
+
+variable "email" {
+  default = "swapnilw611@outlook.com"
+}
